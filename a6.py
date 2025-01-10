9805bf1b5558c34cdb84f89fa9f119513bc97c6e
mherrera63@cps.edu <mherrera63@cps.edu>
2025-01-10 09:58:18 -0600

    worked on a6 and stuff.csv

--- a6.py
+++ a6.py
@@ -7,33 +7,33 @@ data = pd.read_csv("stuff.csv")
 x = data["observation_date"].values
 y = data["GFDEBTN"].values
 
-# Use reshape to turn the x values into 2D arrays:
-x = x.reshape(-1,1)
-
 # Create the model
-model = LinearRegression().fit(x,y)
+# model = LinearRegression().fit(x,y)
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
 
-coef = round(float(model.coef_[0]), 2)
-intercept = round(float(model.intercept_), 2)
-r_squared = model.score(x, y)
+# coef = round(float(model.coef_[0]), 2)
+# intercept = round(float(model.intercept_), 2)
+# r_squared = model.score(x, y)
 
-# # Predict the the blood pressure of someone who is 43 years old.
-# # Print out the prediction
+# Predict the the blood pressure of someone who is 43 years old.
+# Print out the prediction
 # prediction = model.predict([[43]])
 # x_predict = 43
 # print()
 
-# # Print out the linear equation and r squared value
+# Print out the linear equation and r squared value
 # print(f"Model's Linear Equation: y = {coef}x + {intercept}")
 # print(f"R Squared value: {r_squared}")
 # print(f"Prediction when x is {x_predict}: {prediction}")
 
+# labe the axes
+plt.xlabel("Observation Date")
+plt.ylabel("Debt")
 
-# # Create the model in matplotlib and include the line of best fit
-# plt.figure(figsize = (6,4))
-# plt.scatter(x,y, c="purple")
+# Create the model in matplotlib and include the line of best fit
+plt.figure(figsize = (6,4))
+plt.scatter(x,y, c="purple")
 # plt.scatter(43, prediction, c = 'blue')
 
 
